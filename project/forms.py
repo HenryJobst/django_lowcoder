0d0977585ba4f85fb1d4d9507b00e27cf594410c
Henry Jobst <github@jobst-berlin.de>
2022-12-17 20:52:56 +0100

    register form & template via crispy form

--- project/forms.py
+++ project/forms.py
@@ -1,19 +1,31 @@
-from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Fieldset
+from crispy_bootstrap5.bootstrap5 import FloatingField
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False,
-                                 help_text='Optional', label=_('Vorname'))
-    last_name = forms.CharField(max_length=30, required=False,
-                                help_text='Optional', label=_('Nachname'))
-    email = forms.EmailField(max_length=254, help_text=_('Gib bitte eine '
-                                                       'gültige '
-                                                       'E-Mail-Adresse '
-                                                       'an.'), label=_(
-        'E-Mail-Adresse')),
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'login'
+        self.helper.form_class = 'form-register'
+        self.helper.layout = Layout(
+            Fieldset(
+                _('Nutzerdaten'),
+                FloatingField('username'),
+                FloatingField('first_name'),
+                FloatingField('last_name'),
+                FloatingField('email'),
+                FloatingField('password1'),
+                FloatingField('password2')
+                ),
+            Submit('submit', _('Registrieren')),
+            )
 
     class Meta:
         model = User
@@ -24,4 +36,28 @@ class RegisterForm(UserCreationForm):
             'email',
             'password1',
             'password2',
+            ]
+
+
+class LoginForm(AuthenticationForm):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper(self)
+    #     self.helper.form_method = 'post'
+    #     self.helper.form_action = 'login'
+    #     self.helper.form_class = 'form-signin'
+    #     self.helper.layout = Layout(
+    #         Fieldset(
+    #             _('Anmelden'),
+    #             FloatingField('username'),
+    #             FloatingField('password'),
+    #             ),
+    #         Submit('submit', _('Anmelden')),
+    #         )
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
             ]
