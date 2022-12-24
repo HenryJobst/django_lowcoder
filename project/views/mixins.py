24387047d8025eb615de1b02ea739ecf3e7ad636
Henry Jobst <github@jobst-berlin.de>
2022-12-24 01:34:44 +0100

    mixins.py: improve ModelUserFieldPermissionMixin

--- project/views/mixins.py
+++ project/views/mixins.py
@@ -7,8 +7,16 @@ class ModelUserFieldPermissionMixin(UserPassesTestMixin):
     def get_model_permission_user_field(self):
         return self.model_permission_user_field
 
+    # noinspection PyUnresolvedReferences
     def test_func(self):
-        model_attr = self.get_model_permission_user_field()
         current_user = self.request.user
 
-        return current_user == getattr(self.get_object(), model_attr)
+        model_object = self.get_object()
+        user_holder = self.get_user_holder(model_object)
+
+        model_attr = self.get_model_permission_user_field()
+        return current_user == getattr(user_holder, model_attr)
+
+    # noinspection PyMethodMayBeStatic
+    def get_user_holder(self, model_object):
+        return model_object
