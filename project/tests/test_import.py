88afa86fe59e4f5eb97bc4aee8c8838f94e51ecc
Henry Jobst <github@jobst-berlin.de>
2022-12-31 12:19:09 +0100

    fix test data loading

--- project/tests/test_import.py
+++ project/tests/test_import.py
@@ -1,23 +1,38 @@
 from pathlib import Path
 
-from project.services.importer import Importer, SheetReaderParams
+from project.services.importer import (
+    Importer,
+    SheetReaderParams,
+    DEFAULT_SHEET_NAME_FOR_CSV_FILE,
+)
 
 
 class TestImport:
-    def test_sample_csv(self):
-        importer = Importer(Path("./sample_files/Employee-Sample-Data.csv"))
-        sheet0 = "sheet0"
+    project = "project/tests"
+
+    def test_sample_csv(self, pytestconfig):
+        path = pytestconfig.rootpath.joinpath(
+            Path("project/tests/sample_files/Employee-Sample-Data.csv")
+        )
+        importer = Importer(path)
+        sheet0 = DEFAULT_SHEET_NAME_FOR_CSV_FILE
         df = importer.run(sheet0, SheetReaderParams())
         assert not df.empty
 
-    def test_sample_xlsx(self):
-        importer = Importer(Path("./sample_files/Employee-Sample-Data.xlsx"))
+    def test_sample_xlsx(self, pytestconfig):
+        path = pytestconfig.rootpath.joinpath(
+            Path("project/tests/sample_files/Employee-Sample-Data.xlsx")
+        )
+        importer = Importer(path)
         sheet0 = importer.sheets()[0]
         df = importer.run(sheet0, SheetReaderParams())
         assert not df.empty
 
-    def test_sample_multisheet_xlsx(self):
-        importer = Importer(Path("./sample_files/Testtabelle.xlsx"))
+    def test_sample_multisheet_xlsx(self, pytestconfig):
+        path = pytestconfig.rootpath.joinpath(
+            Path("project/tests/sample_files/Testtabelle.xlsx")
+        )
+        importer = Importer(path)
         sheet0 = importer.sheets()[0]
         df = importer.run(sheet0, SheetReaderParams())
         assert not df.empty
