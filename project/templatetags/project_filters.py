a557a275fac98013331093028ee2e0d7b8624ac4
Henry Jobst <github@jobst-berlin.de>
2023-01-02 19:41:38 +0100

    improve project and field list

--- project/templatetags/project_filters.py
+++ project/templatetags/project_filters.py
@@ -1,6 +1,15 @@
 from django import template
 from django.utils.safestring import mark_safe
 
+from project.models import (
+    TransformationFile,
+    TransformationHeadline,
+    TransformationColumn,
+    Project,
+    Field,
+    Model,
+)
+
 register = template.Library()
 
 """
@@ -30,3 +39,49 @@ def iconbool(value):
         )
 
     return mark_safe(result)
+
+
+@register.filter("sheets_count", is_safe=True)
+def sheets_count(file: TransformationFile) -> int:
+    if isinstance(file, TransformationFile):
+        return file.sheets.count() if file.sheets else 0
+    raise TypeError("Call of sheets_count with an invalid type.")
+
+
+@register.filter("headlines_count", is_safe=True)
+def headlines_count(file: TransformationFile) -> int:
+    if isinstance(file, TransformationFile):
+        return TransformationHeadline.objects.filter(
+            transformation_sheet__transformation_file=file
+        ).count()
+    raise TypeError("Call of headlines_count with an invalid type.")
+
+
+@register.filter("columns_count", is_safe=True)
+def columns_count(file: TransformationFile) -> int:
+    if isinstance(file, TransformationFile):
+        return TransformationColumn.objects.filter(
+            transformation_headline__transformation_sheet__transformation_file=file
+        ).count()
+    raise TypeError("Call of columns_count with an invalid type.")
+
+
+@register.filter("models_count", is_safe=True)
+def models_count(project: Project) -> int:
+    if isinstance(project, Project):
+        return Model.objects.filter(transformation_mapping__project=project).count()
+    raise TypeError("Call of models_count with an invalid type.")
+
+
+@register.filter("fields_count", is_safe=True)
+def fields_count(project: Project) -> int:
+    if isinstance(project, Project):
+        return Field.objects.filter(
+            model__transformation_mapping__project=project
+        ).count()
+    raise TypeError("Call of fields_count with an invalid type.")
+
+
+@register.filter("datatype_as_str", is_safe=True)
+def datatype_as_str(datatype) -> str:
+    return Field.DATATYPES[datatype][1]
