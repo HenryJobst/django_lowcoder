7a4e544921932ac8e148216a291117e38f119d8e
Henry Jobst <github@jobst-berlin.de>
2023-01-24 10:13:46 +0100

    fix error on choosing the right code template according to selected model expander

--- project/services/cookiecutter_template_expander.py
+++ project/services/cookiecutter_template_expander.py
@@ -58,9 +58,9 @@ class CookiecutterConfig:
 
     def init_cookiecutter_template_config(self):
 
-        if self.code_template.pk == 1:  # Java-Template
+        if self.code_template.model_exporter == CodeTemplate.ModelExporterClass.JPA.value:  # Java-Template
             self.init_cookiecutter_java_config()
-        elif self.code_template.pk == 2:  # Django-Template
+        elif self.code_template.model_exporter == CodeTemplate.ModelExporterClass.DJANGO.value:  # Django-Template
             self.init_cookiecutter_django_config()
         else:  # Unknown template
             self.init_cookiecutter_config()
